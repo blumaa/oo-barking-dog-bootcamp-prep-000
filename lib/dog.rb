c8eb3b8730e4ff125d2c6010bcc85a552dc24f53
Aaron Blum <blumaa@gmail.com>
2019-07-12 15:30:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,6 +7,14 @@ class Dog
   def name
     @this_dogs_name
   end
+  
+  def bark=(dogs_bark)
+    @this_dogs_bark = dogs_bark
+  end
+  
+  def bark
+    @this_dogs_bark
+  end
 
 #  def initialize(name, bark="woof!")
 #    @name = name
